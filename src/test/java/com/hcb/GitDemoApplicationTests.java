40a1128c80e7e295c7e6efc943ba7b859cc3dd3e
OneNoth1ng <3602869979@qq.com>
2022-07-30 17:00:25 +0800

    全部提交

---
+++ src/test/java/com/hcb/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

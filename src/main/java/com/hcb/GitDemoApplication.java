4a1c2f245f836d24398d14b8b7752f4ac5c1a0b9
OneNoth1ng <3602869979@qq.com>
2022-07-30 16:40:03 +0800

    测试commit

--- src/main/java/com/hcb/GitDemoApplication.java
+++ src/main/java/com/hcb/GitDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("helloGit!");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 

4a285a0d63a5dde535f1d86104768c84bddc6f6d
OneNoth1ng <3602869979@qq.com>
2022-07-30 17:26:16 +0800

    添加main方法，输出hello,Git!

--- src/main/java/com/hcb/Controller/TestController.java
+++ src/main/java/com/hcb/Controller/TestController.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Service;
 public class TestController {
     private String name;
     private int id;
+
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
 }

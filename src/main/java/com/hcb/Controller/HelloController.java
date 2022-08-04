326ffbfd384c393c5201f9b9c2857a712b5dd1c1
OneNoth1ng <3602869979@qq.com>
2022-08-04 16:17:54 +0800

    修改了GitDemo的代码，让其变成可部署的测试项目，主要在网页返回hello，linux

---
+++ src/main/java/com/hcb/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.hcb.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public String hello(){
+        System.out.println("第一次部署项目");
+        return "Hello,Linux";
+    }
+}
